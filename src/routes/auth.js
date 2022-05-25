ea72f19fd2488fdd39efd340826b7a1d51c5a956
robert <robertishimwe0@gmail.com>
2022-05-25 23:15:43 +0200

    passport fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,11 +4,16 @@ import { UserValidation, LoginValidation } from '../validations';
 import { verifyAuth, verifyLogin } from '../middlewares';
 import authMiddleware from '../middlewares/token';
 import UserController from '../controllers/user';
-import googleController from '../controllers/GoogleSocialController';
+import { cacheToken, getToken } from '../helpers/token';
+// import googleController from '../controllers/GoogleSocialController';
 import facebookController from '../controllers/FacebookSocialController';
+import models from '../database/models';
+import { onSuccess } from '../controllers/GoogleSocialController';
 import auth from '../controllers/auth';
 import '../services/googlePassport';
 import '../services/facebookPassport';
+
+const { User } = models;
 
 const { login, logout, generateToken } = auth;
 const { verifyRefresh } = authMiddleware;
@@ -21,7 +26,40 @@ router.get('/facebook', passport.authenticate('facebook', { scope: 'email' }));
 router.get(
   '/auth/facebook/barefoot',
   passport.authenticate('facebook', { failureRedirect: '/failed' }),
-  facebookController.onSuccessfb
+  async function (req, res) {
+    const { name, id, email, displayName } = req.user;
+    const [newUser] = await User.findOrCreate({
+      where: { facebookId: req.user.id },
+      defaults: {
+        firstName: name.familyName,
+        lastName: name.givenName,
+        userName: displayName,
+        isVerified: true,
+        facebookId: id,
+      },
+    });
+
+    await newUser.update({ isVerified: true });
+
+    const params = {
+      user: { id: newUser.dataValues.id },
+    };
+    const duration = parseInt(process.env.TOKEN_EXPIRE, 10);
+    const refreshDuration = parseInt(process.env.REFRESH_EXPIRE, 10);
+
+    const accessTokenObject = getToken(params.user, duration);
+    accessTokenObject.duration = duration;
+    await cacheToken({ user: params.user, code: 'access' }, accessTokenObject);
+
+    const refreshTokenObject = getToken(params.user, refreshDuration);
+    refreshTokenObject.duration = refreshDuration;
+    await cacheToken(
+      { user: params.user, code: 'refresh' },
+      refreshTokenObject
+    );
+
+    res.redirect(`${process.env.RIDIRECT}/?token=${accessTokenObject.token} `);
+  }
 );
 
 router.get(
@@ -32,10 +70,38 @@ router.get(
 router.get(
   '/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/failed' }),
-  googleController.onSuccess
+  async function (req, res) {
+    const { name, id, email, displayName } = req.user;
+    const [newUser] = await User.findOrCreate({
+      where: { email },
+      defaults: {
+        firstName: name.familyName,
+        lastName: name.givenName,
+        userName: displayName,
+        googleId: id,
+        isVerified: true,
+        email,
+      },
+    });
+
+    await newUser.update({ isVerified: true });
+
+    const params = {
+      user: { id: newUser.dataValues.id, email: newUser.dataValues.email },
+    };
+    const duration = parseInt(process.env.TOKEN_EXPIRE, 10);
+    const refreshDuration = parseInt(process.env.REFRESH_EXPIRE, 10);
+
+    const accessTokenObject = getToken(params.user, duration);
+    accessTokenObject.duration = duration;
+    await cacheToken({ user: params.user, code: 'access' }, accessTokenObject);
+    console.log(accessTokenObject);
+
+    res.redirect(`${process.env.RIDIRECT}/?token=${accessTokenObject.token} `);
+  }
 );
 
-router.get('/social/login', googleController.loginWithGoogle);
+// router.get('/social/login', googleController.loginWithGoogle);
 
 router.post('/login', LoginValidation.validateLogin, verifyLogin, login);
 router.get('/logout', verifyAuth, logout);
